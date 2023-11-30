5d0eeaa7a97561672ca50af2388612e8d6dbed46
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2023-11-30 18:50:50 +0530

    Update heap_memory_usage.sh

--- Scripts/heap_memory_usage.sh
+++ Scripts/heap_memory_usage.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 printf "%15s %15s %15s %15s\n" "" "Heap Size" "Heap Alloc" "Heap Free"
 printf "%15s %15s %15s %15s\n" "" "---------" "----------" "---------"
 adb shell dumpsys meminfo com.example.githubactionssampleproject3 | grep "Native Heap" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "Native Heap", $8, $9, $10}'
