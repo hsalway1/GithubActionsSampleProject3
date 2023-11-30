6d6b439f4a3b7f9a2751b537354de53513794534
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2023-11-30 18:13:45 +0530

    Update heap_memory_usage.sh

--- Scripts/heap_memory_usage.sh
+++ Scripts/heap_memory_usage.sh
@@ -1,5 +1,5 @@
 printf "%15s %15s %15s %15s\n" "" "Heap Size" "Heap Alloc" "Heap Free"
 printf "%15s %15s %15s %15s\n" "" "---------" "----------" "---------"
-adb shell dumpsys meminfo com.example.githubactionssampleproject | grep "Native Heap" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "Native Heap", $8, $9, $10}'
-adb shell dumpsys meminfo com.example.githubactionssampleproject | grep "Dalvik Heap" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "Dalvik Heap", $8, $9, $10}'
-adb shell dumpsys meminfo com.example.githubactionssampleproject | grep "TOTAL" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "TOTAL", $8, $9, $10}'
+adb shell dumpsys meminfo com.example.githubactionssampleproject3 | grep "Native Heap" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "Native Heap", $8, $9, $10}'
+adb shell dumpsys meminfo com.example.githubactionssampleproject3 | grep "Dalvik Heap" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "Dalvik Heap", $8, $9, $10}'
+adb shell dumpsys meminfo com.example.githubactionssampleproject3 | grep "TOTAL" | head -1 | awk '{printf "%15s %15d %15d %15d\n", "TOTAL", $8, $9, $10}'
